a342b52c1fac0315937b839b9b98fcd4e3c2407f
Richard Tillies <rtillies@gmail.com>
2022-01-11 11:16:06 -0500

    Person class and spec

---
+++ spec/person_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require 'pry'
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+
+RSpec.describe Person do
+  describe 'Iteration 1' do
+    skip before :each do
+    end
+
+    xit 'exists' do
+    end
+
+    xit 'has attributes' do
+    end
+  end
+end
